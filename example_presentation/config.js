b0b23e933df08d95eb0cdd954678f24bf88a1329
Jem Young <jemuel.young@gmail.com>
2015-07-24 00:49:47 -0400

    init

---
+++ example_presentation/config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  width: 1000,
+  height: 700,
+  margin: 40,
+  theme: require("../themes/default/index"),
+  print: require("../themes/default/print"),
+  html: require("../themes/default/html")
+};
