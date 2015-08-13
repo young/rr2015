4e5d42f8e9658a9378c649b9777b347526ad93b4
Jem Young <jemuel.young@gmail.com>
2015-08-12 22:43:43 -0400

    class

--- examples/example.js
+++ examples/example.js
@@ -13,38 +13,44 @@ const styles = {
   }
 };
 
-const EgoBoost5000 = React.createClass({
-  propTypes: {
+class EgoBoost5000 extends React.Component {
+  static propTypes = {
     fontStyle: PropTypes.object
-  },
-  getInitialState: function() {
-    return {userName: 'Jem'};
-  },
+  }
+
+  constructor() {
+    super();
+    this.state = {userName: 'Jem'};
+  }
+
   updateState(payload) {
     const name = payload.name || 'stranger';
     this.setState({
       userName: name
     });
-  },
+  }
+
   componentDidMount() {
     AppStore.addChangeListener(this.updateState);
-  },
+  }
+
   componentWillUnmount() {
     AppStore.removeChangeListener(this.updateState);
-  },
+  }
+
   render() {
     const blendedStyle = _.extend(styles.fontStyle, this.props.fontStyle);
     return (
       <div>
         <ColorChanger>
           <div>You're awesome,
-            <span style={blendedStyle}>{this.state.name}</span>
+            <span style={blendedStyle}>{this.state.userName}</span>
           </div>
         </ColorChanger>
         <Footer />
       </div>
     );
   }
-});
+}
 
 export default EgoBoost5000;
