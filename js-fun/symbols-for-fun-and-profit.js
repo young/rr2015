fe34752da5b7612b189d1c5da93617f29a9ba44f
Jem Young <jem@chartbeat.com>
2015-08-23 15:04:32 -0400

    symbol wip

---
+++ js-fun/symbols-for-fun-and-profit.js
@@ -0,0 +1,10 @@
+/**
+ * @description Use Symbols to quickly iterate over an object
+ *
+ * Symbols have a unqiue property wherein they are skipped over when using a
+ * for-of loop. If we use a symbol an object key, it will be skipped over 
+ */
+
+(function() {
+
+})();
