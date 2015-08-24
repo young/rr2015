453b04d4ee669b3fd0e51a2c35728ca75eadf8de
Jem Young <jem@chartbeat.com>
2015-08-24 10:08:06 -0400

    Js fun

--- js-fun/symbols-for-fun-and-profit.js
+++ js-fun/symbols-for-fun-and-profit.js
@@ -1,10 +1,47 @@
 /**
  * @description Use Symbols to quickly iterate over an object
  *
- * Symbols have a unqiue property wherein they are skipped over when using a
- * for-of loop. If we use a symbol an object key, it will be skipped over 
+ * Symbols have a unqiue characteristc wherein they are skipped over when using
+ * a for-of loop. If we use a symbol as an object key, it will be skipped
+ * over allowing us store arbitraily large amounts of data on an object
+ * without any sort of iteration penalty.
+ *
+ *
+ * Time to iterate over 10,000,000 keys:
+ *  Chrome:
+ *    Standard keys: 2295.003ms
+ *    Symbol keys: 389.066ms
+ *
+ *  Firefox:
+ *  Standard keys: 2270.73ms
+ *  Symbol keys: 41.96ms
  */
 
 (function() {
+  'use strict';
 
+  let obj = {};
+  // Build object with standard keys
+  for (var i = 0; i < 1e7; i++) {
+    obj[i] = 1;
+  }
+
+  console.time('Standard keys');
+  for (let x in obj) {}
+  console.timeEnd('Standard keys');
+
+})();
+
+(function() {
+  'use strict';
+
+  let obj = {};
+  // Build object with Symbol keys
+  for (var i = 0; i < 1e7; i++) {
+    obj[Symbol()] = 1;
+  }
+
+  console.time('Symbol keys');
+  for (let x in obj) {}
+  console.timeEnd('Symbol keys');
 })();
